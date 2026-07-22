64b2fdd325d664c67ecdc904514d3c46972906d4
PSYCO_ASTRO <ishaan12p@gmail.com>
2026-07-23 02:12:48 +0530

    Binary Search addition

--- src/BinarySearch.java
+++ src/BinarySearch.java
@@ -9,7 +9,7 @@ public class BinarySearch {
         // their addition
 
         // Use formula : low + (high - low ) / 2;
-        System.out.println("Binary Search Implementation: Enter 10 random no");
+        System.out.println("Binary Search Implementation: Enter 5 random no");
         Scanner sc = new Scanner(System.in);
 
         int Binaryarr[] = new int[10];
@@ -17,6 +17,47 @@ public class BinarySearch {
             Binaryarr[i] = sc.nextInt();
         }
         Arrays.sort(Binaryarr);
+        for ( int num : Binaryarr){
+            System.out.print(num + " ");
+        }
+
+        System.out.print("\n ENTER THE NUMBER TO SEARCH");
+        int target = sc.nextInt();
+
+        int low = 0;
+        int  high = Binaryarr.length - 1;
+        int answer = -1;
+
+        while ( low <= high ){
+            int mid = low + (high - low)/2;
+
+            if (Binaryarr[mid] == target){
+                System.out.println("Element found at Index" + mid);
+                // I am modifying this code to find the first occurance of 2 if their are multiple 2 in sorted the array
+                //return;
+                answer = mid;
+                high = mid - 1;
+
+                // for last occurance
+                // answer = mid;
+                // low =  mid + 1;
+            } else if (Binaryarr[mid] < target) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        if (answer != -1){
+           System.out.println("First Occurance " + answer);
+        }
+        else{
+            System.out.println("Element not found");
+        }
+
+        // count occurances :   last occurance - first occurance + 1
+
+        sc.close();
+
 
 
 
